f6a5272fb5647b5d3227ad6612f43e41e2022a18
Ferran Maylinch <ferran@yandex-team.ru>
2024-01-20 01:54:52 +0300

    Add route /chat

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # OpenAI API
 
-To be used as a proxy to api.openai.com
+To be used as a proxy to https://api.openai.com
+
+Followed instructions from
+- https://masteringbackend.com/posts/how-to-deploy-your-node-js-backend-project-to-vercel-a-step-by-step-guide
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // index.js
 const express = require('express')
 const app = express()
+app.use(express.json()) // parse json bodies
 const PORT = 4000
+const OpenAI = require('openai')
 
-
-app.get('/home', (req, res) => {
-    res.status(200).json('Welcome, your app is working well');
+app.get('/', async (req, res) => {
+    res.status(200).json('App is running');
 })
 
+app.post('/chat', async (req, res) => {
+    console.log("request body", req.body);
+
+    let openai = new OpenAI({
+        apiKey: req.body.apiKey
+    });
+
+    let messages = [
+        {role: "user", content: req.body.message}
+    ]
+
+    const response = await openai.chat.completions.create({
+        model: req.body.model || "gpt-4",
+        messages
+    });
+
+    res.status(200).json(response);
+})
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
